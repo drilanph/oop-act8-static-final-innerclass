a6be3df71a7e59b5ae79af134d174fbcf152763e
drilanph <drilan.ph14@gmail.com>
2017-07-10 15:31:11 +0800

    comit 1

---
+++ Argoncillo_071017_act8/src/Argoncillo_Task1/InnerClass.java
@@ -0,0 +1,16 @@
+
+package Argoncillo_Task1;
+
+
+public class InnerClass {
+ 
+
+  int data2 = 10;
+  void method(OuterClass a){
+      OuterClass oc = new OuterClass();
+      
+      System.out.println(oc.data);
+      System.out.println(data2);
+  
+  }
+}
---
+++ Argoncillo_071017_act8/src/Argoncillo_Task1/OuterClass.java
@@ -0,0 +1,7 @@
+
+package Argoncillo_Task1;
+
+
+public class OuterClass {
+int data = 5;
+}
